6ea24b69b419361d926a2e89f3a2c1f099be1ff7
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-04-15 15:09:37 +0900

    [feat] Switch Page (#9)
    - 페이지 이동 기능 구현
    - 각 페이지 별로 파일 생성
    - 기본 화면의 파일명은 Report Page 이지만 유저 편의상 Navigation Bar 의 명칭은 Home 으로 설정
    - 별 다른 상태관리 도구를 사용하지 않고 setState로 구현했기에 추후 개선 필요

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({super.key});
+  void Function(int)? onTabChange;
+   MyBottomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GNav(tabs: [
+      padding: EdgeInsets.symmetric(vertical: 12),
+      child: GNav(
+        tabBackgroundColor: Colors.grey.shade100, // 선택된 부분 뒷 배경색
+        tabActiveBorder: Border.all(color: Colors.white), // 선택된 부분 테두리 색
+        activeColor: Colors.grey.shade700, // 선택된 부분 아이콘 색
+        color: Colors.grey, // 선택되지 않은 부분 아이콘 색
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        padding: EdgeInsets.all(20),
+
+        onTabChange: (value) => onTabChange!(value), // 탭이 바뀌면 onTabChange 함수 호출
+
+        tabs: [
         GButton(
           icon: Icons.home,
           text: 'Home',
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,55 @@
+// 인트로 페이지에서 들어가기 누르면 나오는 페이지
+// 다른 페이지들로 이동할 수 있는 버튼이 있는 페이지
+
 import 'package:flutter/material.dart';
+import 'package:flutter_book_report_app/pages/book_page.dart';
+import 'package:flutter_book_report_app/pages/profile_page.dart';
+import 'package:flutter_book_report_app/pages/report_page.dart';
+import 'package:flutter_book_report_app/pages/search_page.dart';
 
 import '../components/bottom_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom navigation bar
+  int _selectedIndex = 0;
+
+  // this method is to change the selected index
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // page to display
+  final List<Widget> _pages = [
+    // Home page
+    const ReportPage(),
+
+    // Search page
+    const SearchPage(),
+
+    // Book page
+    const BookPage(),
+
+    // Profile page
+    const ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: MyBottomNavBar(),
+      backgroundColor: Color(0xffece6d2),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
