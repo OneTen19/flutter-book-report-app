a032c55196b01766c3156f314413b5cfbd8c1db2
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-04-16 14:50:56 +0900

    [feat] Drawer Menu (#11)
    - Drawer 패키지를 이용해 Menu 화면 구현

---
+++ lib/pages/drawer_page.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class DrawerPage extends StatelessWidget {
+  const DrawerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('data');
+  }
+}
---
+++ lib/theme/colors.dart
@@ -0,0 +1,6 @@
+// 앱에서 반복적으로 사용되는 색상을 기록해두는 곳
+// 다크모드등의 앱 자체 테마색을 변경하는 기능 구현 시 유용하게 사용
+
+import 'package:flutter/material.dart';
+
+const defaultAppBackgroundColor = Color(0xffece6d2);
