74969567476c3a0f770e0f97841ee38221f8c1d4
OneTen19 <khy000426@naver.com>
2024-04-16 14:41:05 +0900

    [feat] Drawer Menu
    - Drawer 패키지를 이용해 Menu 화면 구현

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_book_report_app/pages/book_page.dart';
 import 'package:flutter_book_report_app/pages/profile_page.dart';
 import 'package:flutter_book_report_app/pages/report_page.dart';
 import 'package:flutter_book_report_app/pages/search_page.dart';
+import 'package:flutter_book_report_app/theme/colors.dart';
 
 import '../components/bottom_nav_bar.dart';
 
@@ -45,9 +46,92 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffece6d2),
+      backgroundColor: defaultAppBackgroundColor,
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: const Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.orange.shade200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                // profile image
+                // DrawerHeader를 이용해 프로필 이미지를 구현
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: DrawerHeader(
+                    margin: EdgeInsets.all(8),
+                    child: Image.asset(
+                      'assets/images/profile_image.jpg',
+                    ),
+                  ),
+                ),
+
+                // Home
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(Icons.home, color: Colors.white),
+                    title: const Text('Home',
+                        style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      navigateBottomBar(0);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+
+                // About
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: const Icon(Icons.info, color: Colors.white),
+                    title: const Text('About',
+                        style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      navigateBottomBar(1);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+            // Logout
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: ListTile(
+                leading: const Icon(Icons.logout, color: Colors.white),
+                title:
+                    const Text('Logout', style: TextStyle(color: Colors.white)),
+                onTap: () {
+                  navigateBottomBar(2);
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       body: _pages[_selectedIndex],
     );
